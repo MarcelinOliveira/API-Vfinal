b171a0ded320c5af27c77c666eeec28e9f76caa7
MarcelinOliveira <104645737+MarcelinOliveira@users.noreply.github.com>
2024-04-22 21:33:18 -0300

    Add files via upload

---
+++ APIUploadImagem/WebApplication1/Controllers/FileController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using WebApplication1.Helpers;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<ActionResult> Upload([FromForm] ICollection<IFormFile> files)
+        {
+            try
+            {
+                if (files == null || files.Count == 0)
+                    return BadRequest();
+                List<byte[]> data = new();
+                foreach (var formfile in files)
+                {
+                    if (formfile.Length > 0)
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            await formfile.CopyToAsync(stream);
+                            data.Add(stream.ToArray());
+                        }
+                    }
+                }
+                var slvImg = new SalvaImagem("C:\\Users\\junio\\Desktop\\TCC\\Imagens");
+                foreach (var file in data)
+                {
+                    slvImg.SalvarImagem(file);
+                }
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+            
+        }
+    }   
+}
---
+++ APIUploadImagem/WebApplication1/Helpers/SalvaImagem.cs
@@ -0,0 +1,36 @@
+﻿namespace WebApplication1.Helpers
+{
+    public class SalvaImagem        
+    {
+        public string pathimagenss { get; set; }
+        public SalvaImagem(string pathimagens)
+        {
+            try
+            {
+                pathimagenss = pathimagens;
+                // Certifique-se de que o diretório de saída existe, ou crie-o se não existir
+                if (!Directory.Exists(pathimagens))
+                {
+                    Directory.CreateDirectory(pathimagens);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocorreu um erro ao salvar a imagem: " + ex.Message);
+            }
+        }
+
+        public void SalvarImagem(byte[] imagebytes)
+        {
+            string filename = "Capim_Amargoso" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + ".jpg";
+
+            // Combine o diretório de saída com o nome do arquivo para obter o caminho completo
+            string filePath = Path.Combine(pathimagenss, filename);
+
+            // Escreva os bytes no arquivo
+            File.WriteAllBytes(filePath, imagebytes);
+
+            Console.WriteLine("Imagem salva com sucesso em: " + filePath);
+        }
+    }
+}
